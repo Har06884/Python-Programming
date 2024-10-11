669f3fb302dbfd29ce1c781b59ed64606f44d0f6
Har06884 <christopher.harrison@coleggwent.ac.uk>
2024-10-11 11:58:52 +0100

    Create Sets.py

---
+++ Sets.py
@@ -0,0 +1,19 @@
+# Create a set
+my_set = {1, 2, 3, 4, 5}
+print("Initial set:", my_set)
+
+# Display items in the set
+for item in my_set:
+    print(item)
+
+# Add an item to the set
+my_set.add(6)
+print("Set after adding an item:", my_set)
+
+# Remove an item from the set
+my_set.remove(3)
+print("Set after removing an item:", my_set)
+
+# Clear the set
+my_set.clear()
+print("Set after clearing all items:", my_set)
